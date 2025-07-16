9dbce817b73cd075bdae60eeee290f722ae8aa14
Zeiad Hatem <zeiadzezozeiad2007@gmail.com>
2025-07-16 19:09:14 +0300

    z

--- DEFS.h
+++ DEFS.h
@@ -1,7 +1,7 @@
 #ifndef DEFS_H
 #define DEFS_H
 
-
+//bolbol
 //This file contais some global constants and definitions to be used in the project.
 enum operationType //The operations supported (you can add more if needed)
 {
