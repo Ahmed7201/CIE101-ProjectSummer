24ee31ba41f4bafaf9763367a5878379b8644f9b
abdal <abdal@ABDULLAH-LAPTOP>
2025-07-16 19:19:31 +0300

    C++

--- DEFS.h
+++ DEFS.h
@@ -1,7 +1,7 @@
 #ifndef DEFS_H
 #define DEFS_H
-
-//bolbol
+//I love C++ and Toosa;
+//bolbol;
 //This file contais some global constants and definitions to be used in the project.
 enum operationType //The operations supported (you can add more if needed)
 {
