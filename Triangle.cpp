3ecef9b10a98328b6448a48c8b84ca6d0331d555
abdal <abdal@ABDULLAH-LAPTOP>
2025-07-25 21:44:21 +0300

    Fixed something in Triangle.cpp

--- Triangle.cpp
+++ Triangle.cpp
@@ -20,9 +20,17 @@ bool Triangle::isInside(int x, int y) const
 {
 	// Check if the point (x,y) is inside the triangle using barycentric coordinates
 	double area = 0.5 * (-Corner2.y * Corner3.x + Corner1.y * (-Corner2.x + Corner3.x) + Corner1.x * (Corner2.y - Corner3.y) + Corner2.x * Corner3.y);
-	double s = 1 / (2 * area) * (Corner1.y * Corner3.x - Corner1.x * Corner3.y + (Corner3.y - Corner1.y) * x + (Corner1.x - Corner3.x) * y);
-	double t = 1 / (2 * area) * (Corner1.x * Corner2.y - Corner1.y * Corner2.x + (Corner1.y - Corner2.y) * x + (Corner2.x - Corner1.x) * y);
-	return s >= 0 && t >= 0 && (s + t <= 1);
+	if (area == 0)
+	{
+		return false; 
+	}
+	else
+	{
+		double s = 1 / (2 * area) * (Corner1.y * Corner3.x - Corner1.x * Corner3.y + (Corner3.y - Corner1.y) * x + (Corner1.x - Corner3.x) * y);
+		double t = 1 / (2 * area) * (Corner1.x * Corner2.y - Corner1.y * Corner2.x + (Corner1.y - Corner2.y) * x + (Corner2.x - Corner1.x) * y);
+		return s >= 0 && t >= 0 && (s + t <= 1);
+	}
+	
 }
 string Triangle::GetShapeType() const
 {
