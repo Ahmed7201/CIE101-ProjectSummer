cb6ef3eb4c17c65f090db1c96be034ecac0af387
abdal <abdal@ABDULLAH-LAPTOP>
2025-08-05 23:36:43 +0300

    EDited

--- opStickImage.cpp
+++ opStickImage.cpp
@@ -13,14 +13,14 @@ void opStickImage::Execute()
 	GUI* pUI = pControl->GetUI();
 	Graph* pGraph = pControl->getGraph();
 
-	shape* selectedShape = pGraph->GetSelectedShape();
+	shape* clickedShape = pGraph->GetSelectedShape();
 
-	if (!selectedShape)
+	if (!clickedShape)
 	{
 		pUI->PrintMessage("No shape selected. Please select a shape first.");
 		return;
 	}
-	if (selectedShape->HasImage())
+	if (clickedShape->HasImage())
 	{
 		pUI->PrintMessage("The selected shape already has an image.");
 		return;
@@ -32,7 +32,7 @@ void opStickImage::Execute()
 
 	string fullPath = "images\\Shape_Images\\" + filename;
 
-	selectedShape->SetImagePath(fullPath);
+	clickedShape->SetImagePath(fullPath);
 
 	pUI->PrintMessage("Image added to the selected shape.");
 }
